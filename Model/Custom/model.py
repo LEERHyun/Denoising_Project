9339d45199da7e52a8e75e60568c6d2413fbb057
LEERHyun <35624406+LEERHyun@users.noreply.github.com>
2025-10-21 12:17:25 +0900

    Update model.py

--- Model/Custom/model.py
+++ Model/Custom/model.py
@@ -77,14 +77,14 @@ class FeedForward(nn.Module):
 
         self.project_in = nn.Conv2d(dim, hidden_features*2, kernel_size=1, bias=bias)
 
+        self.sg = SimpleGate()
         self.dwconv = nn.Conv2d(hidden_features*2, hidden_features*2, kernel_size=3, stride=1, padding=1, groups=hidden_features*2, bias=bias)
 
         self.project_out = nn.Conv2d(hidden_features, dim, kernel_size=1, bias=bias)
 
     def forward(self, x):
         x = self.project_in(x)
-        x1, x2 = self.dwconv(x).chunk(2, dim=1)
-        x = F.gelu(x1) * x2
+        x= self.sg(x)
         x = self.project_out(x)
         return x
 
@@ -396,3 +396,4 @@ if __name__ == '__main__':
     #torchsummary.summary(res, inp_shape)   
     print('{:<30}  {:<8}'.format('Computational Complexity: ', flops))
     print('{:<30}  {:<8}'.format('Computational Complexity: ', params))
+
